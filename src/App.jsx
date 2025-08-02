64897a247b53c5a577ab903bda4381bb4d0ec9cb
kalamazaad <abdulkalamazaad2447@gmail.com>
2025-08-02 16:34:14 +0530

    v5

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import "./App.css";
 class App extends Component {
+  menuClick(){
+    const navlist=document.getElementById("navlist");
+    navlist.classList.toggle("active");
+  }
   render() {
     return (
      <>
        <header>
            <nav><div className='logo'>My Website</div>
-           <ul>
+           <ul id="navlist">
             <li>Home </li>
             <li>About</li>
             <li>Services</li>
             <li>Contact Us</li>
            </ul>
+           <div className='menu' onClick={()=>this.menuClick()}>&#9776;</div>
            </nav>
        </header>
        <section>
